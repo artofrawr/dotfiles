378f5e9706601b6d044096c1706a487144b9395b
Jens Fischer <jens@weareprofound.com>
2020-10-14 16:04:33 -0400

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# prep
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+PYTHON_VERSION='3.8.6'
+source $SCRIPTPATH/utils.sh
+
+# -------------------------
+# HOMEBREW 
+# -------------------------
+# print_in_purple "HOMEBREW \n"
+
+# if ! cmd_exists "brew"; then
+#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# fi
+# brew update
+# brew upgrade
+# print_success "homebrew installed \n"
+
+# while read p; do
+#   brew install $p
+# done <$SCRIPTPATH/apps/brew.txt
+# print_success "homebrew packages installed \n"
+
+# while read p; do
+#   brew cask install $p
+# done <$SCRIPTPATH/apps/casks.txt
+# print_success "homebrew casks installed \n"
+
+# -------------------------
+# SYMLINKS 
+# -------------------------
+# print_in_purple "S \n"
+# cd $SCRIPTPATH
+
+# backupIfNotSymlink ~/.zshrc
+# backupIfNotSymlink ~/.gitconfig
+# backupIfNotSymlink ~/.zsh
+# backupIfNotSymlink ~/Library/Application\ Support/Code/User/settings.json
+
+# git clone https://github.com/seebi/dircolors-solarized ~/.dircolors-solarized
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# ln -v -s ~/.dircolors-solarized/dircolors.ansi-dark ~/.dircolors
+# ln -v -s $SCRIPTPATH/configs/zshrc ~/.zshrc
+# ln -v -s $SCRIPTPATH/configs/vscode.json ~/Library/Application\ Support/Code/User/settings.json
+# cp -v $SCRIPTPATH/configs/gitconfig ~/.gitconfig
+
+
+# -------------------------
+# PYTHON 
+# -------------------------
+print_in_purple "PYTHON \n"
+version=$(python -V 2>&1)
+if [[ $version != "Python $PYTHON_VERSION" ]]; then
+  pyenv install $PYTHON_VERSION
+  pyenv global $PYTHON_VERSION
+fi
+print_success "python installed \n"
---
+++ utils.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+backupIfNotSymlink() {
+    if [[ -L $1 ]]; then
+        rm -v "$1"
+    elif [[ -e $1 ]]; then
+        mv -v -f "$1" "$1.bak"
+    fi
+}
+
+cmd_exists() {
+    command -v "$1" &> /dev/null
+}
+
+print_error() {
+    print_in_red "   [✖] $1 $2\n"
+}
+
+print_error_stream() {
+    while read -r line; do
+        print_error "↳ ERROR: $line"
+    done
+}
+
+print_in_color() {
+    printf "%b" \
+        "$(tput setaf "$2" 2> /dev/null)" \
+        "$1" \
+        "$(tput sgr0 2> /dev/null)"
+}
+
+print_in_green() {
+    print_in_color "$1" 2
+}
+
+print_in_purple() {
+    print_in_color "$1" 5
+}
+
+print_in_red() {
+    print_in_color "$1" 1
+}
+
+print_in_yellow() {
+    print_in_color "$1" 3
+}
+
+print_question() {
+    print_in_yellow "   [?] $1"
+}
+
+print_result() {
+
+    if [ "$1" -eq 0 ]; then
+        print_success "$2"
+    else
+        print_error "$2"
+    fi
+
+    return "$1"
+
+}
+
+print_success() {
+    print_in_green "   [✔] $1\n"
+}
+
+print_warning() {
+    print_in_yellow "   [!] $1\n"
+}
